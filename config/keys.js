d8fe5b61aff2f7f66320b57cc3874efc48eea935
wowbob396 <wowbob396@gmail.com>
2018-09-16 22:49:21 -0500

    connected server to mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://bran:bran123@ds159772.mlab.com:59772/course-organizer-augsburg'
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// bodyparser middlewaret
+app.use(bodyParser.json())
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongo
+
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch( err => console.error(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
