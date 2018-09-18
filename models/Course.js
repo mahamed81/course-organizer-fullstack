0d6ba2126320cddb99a7454741648f864661e76b
wowbob396 <wowbob396@gmail.com>
2018-09-17 19:46:32 -0500

    created post request for new courses

--- models/Course.js
+++ models/Course.js
@@ -7,6 +7,9 @@ const CourseSchema = new Schema({
         type: String,
         required: true
     },
+    courseDep: {
+        type: String
+    },
     courseNum: {
         type: Number
     },
--- routes/api/courses.js
+++ routes/api/courses.js
@@ -5,12 +5,26 @@ const router = express.Router();
 const Course = require('../../models/Course');
 
 // @route GET api/courses
-// @desc Get all items
+// @desc Get all courses
 // @access Public 
 router.get('/', (req,res) => {
     Course.find()
         .sort({date: -1})
         .then(courses => res.json(courses))
 });
+
+// @route Post api/courses
+// @desc Create a course
+// @access Public 
+router.post('/', (req,res) => {
+    const newCourse = new Course({
+        name: req.body.name,
+        courseNum: req.body.courseNum,
+        courseDep: req.body.courseDep
+    });
+
+    newCourse.save().then(course => res.json(course));
+});
+
 
 module.exports = router;
