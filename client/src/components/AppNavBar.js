f0d8dde8fba64385e687f33486cb17d79f7c9d9a
wowbob396 <wowbob396@gmail.com>
2018-09-26 19:30:27 -0500

    merged conflicts with navbar

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 
 import {
-<<<<<<< HEAD
   Button, Icon,
-=======
-    Sidebar,
-    Button
->>>>>>> b0c60c8c08cd7470429a622a98f7bd121ae1e971
 } from 'semantic-ui-react'
 
 /**
@@ -26,21 +21,11 @@ export default class AppNavBar extends Component {
     render() {
 
         return (
-<<<<<<< HEAD
-        <Button.Group>
-        
-            <Button><Icon name="home"/>Home</Button>
-            <Button><Icon name="file alternate"/>Transcript</Button>
-                <Button><Icon  name="book" />Courses</Button>
-        </Button.Group>
-        
-=======
             <div>
                 
 
                 
             </div>
->>>>>>> b0c60c8c08cd7470429a622a98f7bd121ae1e971
         )
     }
   
