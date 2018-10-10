329d4f7d802c2b87b901c0e5f219e5d15b29e970
wowbob396 <wowbob396@gmail.com>
2018-10-10 18:41:01 -0500

    merged catalog conflicts

--- client/src/components/Catalog.js
+++ client/src/components/Catalog.js
@@ -16,54 +16,6 @@ const mapStateToProps = state => {
     return { courses: state.courses };
 };
 
-<<<<<<< HEAD
-const createPrereqCard = prereq => {
-    return <Card fluid color="grey">
-        <Card.Content>
-      <Card.Header>{prereq.name}</Card.Header>
-      <Card.Meta>{prereq.department + " " + prereq.num}</Card.Meta>
-      <Card.Description >
-          {prereq.description}
-      </Card.Description>
-  </Card.Content>
-    </Card>
-}
-
-/**
- * This method takes in a course object, and returns a card that is displayed to the page
- * 
- */
-const createCourseCard = course => {
-    //let prereqs = course.prereq.map(prereq => {
-    //  return createPrereqCard(prereq)
-    //});
-  return <Card fluid color="grey">
-  <Card.Content>
-      <Card.Header>{course.CourseTitle}</Card.Header>
-      <Card.Meta>{course.CourseDepartmentShort + " " + course.CourseNumber}</Card.Meta>
-      <Card.Description >
-
-      </Card.Description>
-      <Breadcrumb>
-          <Modal trigger={<Breadcrumb.Section className="prereq" link><Icon name="archive"/>Prequisite(s)</Breadcrumb.Section>}>
-              <Modal.Content>
-              <h3>Prequisite(s)</h3>
-               <Card.Group>{course.CoursePrerequisite}</Card.Group>
-              </Modal.Content>
-          </Modal>
-          <Breadcrumb.Divider icon='right angle'/>
-          <Modal trigger={<Breadcrumb.Section className="prereq" link><Icon name="align left"/>Description</Breadcrumb.Section>}>
-              <Modal.Content>
-              <h3>Course Description</h3>
-              <div>{course.CourseDescription}</div>
-              </Modal.Content>
-          </Modal>
-      </Breadcrumb>
-  </Card.Content>
-  </Card>
-}
-=======
->>>>>>> dev
 
  const ConnectedCatalog = (props) => {
 
